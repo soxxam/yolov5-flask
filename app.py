c0e34975bc190d6a20a64f321b779c740e3896c8
soxxam <tutran0904@gmail.com>
2022-10-05 13:34:25 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,69 @@
+"""
+Simple app to upload an image via a web form 
+and view the inference results on the image in the browser.
+"""
+import argparse
+import io
+import os
+from re import L
+from PIL import Image
+import json
+import torch
+from flask import Flask, render_template, request, redirect
+import numpy as np
+import cv2
+import base64
+from datetime import datetime
+
+##############
+import os
+
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+#############
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+        if not file:
+            return
+
+        img_bytes = file.read()
+        img = Image.open(io.BytesIO(img_bytes))
+        results = model(img, size=640)
+
+        # for debugging
+        # data = results.pandas().xyxy[0].to_json(orient="records")
+        # return data
+
+        results.render()  # updates results.imgs with boxes and labels
+        for img in results.imgs:
+            img_base64 = Image.fromarray(img)
+            img_base64.save("static/image0.jpg", format="JPEG")
+        return redirect("static/image0.jpg")
+
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
+    parser.add_argument("--port", default=5000, type=int, help="port number")
+    args = parser.parse_args()
+
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='runs/train/exp/weights/best.pt')
+    # model_bxe = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
+    model.eval()
+    # model_bxe.eval()
+    app.run(host="0.0.0.0", port=args.port, debug = True) # debug=True causes Restarting with stat
